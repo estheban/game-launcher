1d36d05996947af849f4dd55c4b2eb09137a5209
Etienne Lachance <et@etiennelachance.com>
2024-03-19 09:41:58 -0400

    Add backend app

---
+++ backend/tests/Unit/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
